b179564aca79da305ae2e0a86ad8b503524521c9
Aadarsh Sharma <aadarsh4464@gmail.com>
2026-09-03 11:37:05 +0530

    Time: 3 ms (91.3%), Space: 121.5 MB (74.64%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,32 +1,23 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int y = Integer.MAX_VALUE;
-        for(int i = 0; i < nums1.length; i++){
-            if(nums1[i] % 2 == 1 && nums1[i] < y)
-                y = nums1[i];
-        }
-        boolean odd = true;
-        //Odd
-        int[] nums2 = new int[nums1.length];
-        for(int i = 0; i < nums1.length; i++){
-            nums2[i] = nums1[i];
-            if(nums1[i] % 2 == 0)
-                nums2[i] -= y;
-            if(nums2[i] <= 0){
-                odd = false;
-                break; 
+        int min = Integer.MAX_VALUE;
+        for(int num:nums1)
+            min = Math.min(min,num);
+        if((min & 1) == 1){
+            for(int i = 0; i < nums1.length; i++){
+                if((nums1[i] & 1) == 0)
+                    nums1[i] -= min;
+                if(nums1[i] <= 0)
+                    return false;
             }
+            return true;
         }
-        boolean even = true;
-        for(int i = 0; i < nums1.length; i++){
-            nums2[i] = nums1[i];
-            if(nums1[i] % 2 == 1)
-                nums2[i] -= y;
-            if(nums2[i] <= 0){
-                even = false;
-                break; 
+        else{
+            for(int num:nums1){
+                if((num & 1) == 1)
+                    return false;
             }
+            return true;
         }
-        return odd || even;
     }
 }
