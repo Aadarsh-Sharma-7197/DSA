2d12566a77404fdcbb3abed01005af7f149714f6
Aadarsh Sharma <aadarsh4464@gmail.com>
2026-09-18 19:23:17 +0530

    Time: 2 ms (55.85%), Space: 43.2 MB (9.36%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.java
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.java
@@ -1,35 +1,22 @@
 class Solution {
-    public boolean helper(int[] digits,boolean[] used,List<Integer> cur){
-        if(digits.length == cur.size()){
-            int x = 0;
-            for(Integer i:cur)
-                x = (x*10 + i);
-            if(cur.get(0) != 0 && (x & (x-1)) == 0)
-                return true;
-            return false;
+    public String count(int n){
+        int[] freq = new int[10];
+        while(n!=0){
+            freq[n%10]++;
+            n/=10;
         }
-        for(int i = 0; i < digits.length; i++){
-            if(used[i]) continue;
-            if(i > 0 && digits[i] == digits[i-1] && !used[i-1]) continue;
-            cur.add(digits[i]);
-            used[i] = true;
-            if(helper(digits,used,cur))
-                return true;
-            cur.remove(cur.size()-1);
-            used[i] = false;
-        }
-        return false;
+        StringBuilder sb = new StringBuilder();
+        for(int num:freq)
+            sb.append(num).append("#");
+        return sb.toString();
     }
     public boolean reorderedPowerOf2(int n) {
-        int size = (int)Math.log10(n) + 1;
-        int[] digits = new int[size];
-        int i = 0;
-        while(n!=0){
-            digits[i++] = n%10;
-            n /= 10;
+        String target = count(n);
+        for(int i = 1; i < (int)1e9; i*=2){
+            String str = count(i);
+            if(str.equals(target))
+                return true;
         }
-        Arrays.sort(digits);
-        boolean[] used = new boolean[size];
-        return helper(digits,used,new ArrayList<>());
+        return false;
     }
 }
