51e05ee5c8f51d173893f08adca71e71b26ff08a
Aadarsh Sharma <aadarsh4464@gmail.com>
2026-09-27 00:09:39 +0530

    Time: 2 ms (46.41%), Space: 47 MB (18.78%) - LeetHub

--- 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.java
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.java
@@ -1,19 +1,25 @@
 class Solution {
     public int[] limitOccurrences(int[] nums, int k) {
+        int cnt = 1;
         List<Integer> list = new ArrayList<>();
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        for(int num:nums)
-            map.put(num,map.getOrDefault(num,0)+1);
-        for(int key:map.keySet()){
-            int size = Math.min(map.get(key),k);
-            for(int i = 0; i < size; i++){
-                list.add(key);
+        list.add(nums[0]);
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1])
+                cnt++;
+            else
+                cnt = 1;
+            if(cnt > k){
+                while(i < nums.length && nums[i] == nums[i-1])
+                    i++;
+                cnt = 1;
             }
+            if(i < nums.length)
+                list.add(nums[i]);
         }
         int[] ans = new int[list.size()];
-        int p = 0;
+        int x = 0;
         for(int num:list)
-            ans[p++] = num;
+            ans[x++] = num;
         return ans;
     }
 }
